eefb93a7e45b0563c76c91e96619e69fff6f1c05
Ruben <rufopufo@gmail.com>
2018-02-26 22:53:19 +0100

    Creada app con activities

---
+++ AlbergApp/app/src/main/java/com/rubenjavier/as/albergapp/InicioActivity.java
@@ -0,0 +1,13 @@
+package com.rubenjavier.as.albergapp;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class InicioActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_inicio);
+    }
+}
---
+++ AlbergApp/app/src/main/java/com/rubenjavier/as/albergapp/MapActivity.java
@@ -0,0 +1,46 @@
+package com.rubenjavier.as.albergapp;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.mapbox.mapboxsdk.MapboxAccountManager;
+import com.mapbox.mapboxsdk.maps.MapView;
+
+public class MapActivity extends AppCompatActivity {
+    private MapView mapView;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        MapboxAccountManager.start(this, getString(R.string.access_token));
+        setContentView(R.layout.activity_map);
+
+
+        mapView = (MapView) findViewById(R.id.mapview);
+        mapView.onCreate(savedInstanceState);
+    }
+
+
+    @Override
+    public void onPause()  {
+        super.onPause();
+        mapView.onPause();
+    }
+
+    @Override
+    public void onResume() {
+        super.onResume();
+        mapView.onResume();
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        mapView.onDestroy();
+    }
+
+    @Override
+    protected void onSaveInstanceState(Bundle outState) {
+        super.onSaveInstanceState(outState);
+        mapView.onSaveInstanceState(outState);
+    }
+}
